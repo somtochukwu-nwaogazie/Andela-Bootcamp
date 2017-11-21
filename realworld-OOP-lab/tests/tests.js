d84d118e791ab6aa50c1f469d0f4390004d1c4d6
somtochukwu-nwaogazie <somto.nwaogazie@gmail.com>
2017-11-21 16:57:46 +0100

    chore(test):Edit tests

--- realworld-OOP-lab/tests/tests.js
+++ realworld-OOP-lab/tests/tests.js
@@ -1,39 +1,46 @@
 import {expect} from 'chai';
-import Phone from '../src/OOP-challenge.js';
+import {Iphone} from '../src/OOP-challenge.js';
+import {Samsung} from '../src/OOP-challenge.js';
+import {Microsoft} from '../src/OOP-challenge.js';
+// import Phone from '../src/OOP-challenge.js';
 
 
 
-describe("Create a phone class that can make and receive calls and texts", () => {
+describe("Create an Iphone class", () => {
 	it("should be an object", () => {
-        expect(typeof Phone).to.eql(typeof {});
+		const applePhone = new Iphone();
+        expect(typeof applePhone).to.eql(typeof {});
    
 });
 
 
      it("should be an instance of phone", () =>{
-     	const samsung = new Phone('Samsung', 'Model');
-        expect(samsung).to.be.an.instanceof(Phone);
+     	const androidPhone = new Samsung('Samsung', 'S7 Edge');
+        expect(androidPhone).to.be.an.instanceof(Samsung);
 
 
 });
      it("the phone type should default to 'phone'.", () =>{
-       const iphone = new Phone();
-       expect(iphone.type).to.eql('phone');
+       const applePhone = new Iphone('Iphone','6 plus');
+       expect(applePhone.type).to.eql('Iphone');
 
 
 });
      it("the phone type and model should be properties of the phone",() =>{
-     	const microsoft = new Phone();
-        expect(microsoft.type).to.eql('Microsoft');
-        expect(microsoft.model).to.eql('Lumia');
+     	const nokia = new Microsoft('Lumia', '930');
+        expect(nokia.type).to.eql('Lumia');
+        expect(nokia.model).to.eql('930');
 
 
 });
      it("should have a method called photo count that is initialized to zero", () =>{
-     	expect(Phone.photo).to.eql(0);
+     	const androidPhone = new Samsung('Samsung', 'S7 Edge');
+     	expect(androidPhone.photoCount).to.eql(0);
 });  
 
      it("should have a method that when called on, should increase photo count by 1",() =>{
-     	expect(Phone.takePhoto()).to.eql(phone.photo++);
+     	const applePhone = new Iphone('Iphone', '6 plus');
+     	applePhone.takePhoto();
+     	expect(applePhone.photoCount).to.eql(1);
      });
 });
